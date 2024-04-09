6bb40f5956b3890bacd279946ba1054b25a61f55
NULLVOID76 <nullvoid76@gmail.com>
2024-04-09 21:03:01 +0530

    change in to

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ function App() {
     setFinalAmount(amount * currencyInfo[to]);
   };
 
-  useEffect(convert,[from]);
+  useEffect(convert,[to]);
 
   const BackgroundImage =
     "https://images.unsplash.com/photo-1560221328-12fe60f83ab8?q=80&w=1774&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
